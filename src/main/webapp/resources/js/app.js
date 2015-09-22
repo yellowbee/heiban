ed5a0374f464b395ab33931a9da8ba3c0012546f
yunfeng <yfenghuang@yahoo.com>
2015-09-21 21:45:03 -0400

    cross origin resources allowed from rest server

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -5,11 +5,11 @@ var heibanApp = angular.module('heibanApp', []);
 heibanApp.controller('courseCtrl', function ($scope, $http){
 
     function init(){
-        $http.get('http://rubric-elb-5171323.us-east-1.elb.amazonaws.com/rubric/0b4f3936-efe7-43f2-a014-4b780aca4eca')
+        $http.get('http://104.230.217.10:8090/rest/courses')
             .success(function(data) {
-            $scope.courses = data;
-            console.log(data);
-        });
+                $scope.courses = data;
+                console.log(data);
+            });
     }
 
     init();
